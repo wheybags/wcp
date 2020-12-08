8e43236e05bbf03ee005cd007a829be4a5cb11ed
Tom Mason <wheybags@wheybags.com>
2020-12-08 18:27:08 +0000

    benchmark script improvements

--- bench.sh
+++ bench.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 SIZE_MB=512
-FILE_COUNT=2
+FILE_COUNT=20
 
 TEST_DATA="./test_data/$SIZE_MB""_$FILE_COUNT"
 TEST_DEST="./test_dest"
@@ -12,7 +12,7 @@ build() {
     mkdir -p build_test
     cd build_test
     cmake .. -DCMAKE_BUILD_TYPE=Release
-    make -j $(grep -c processor /proc/cpuinfo)
+    make -j "$(grep -c processor /proc/cpuinfo)"
     cd ..
 }
 
@@ -21,7 +21,7 @@ generate_data() {
     local size_in_mb="$2"
     local count="$3"
 
-    for i in $(seq 1 $count); do
+    for i in $(seq 1 "$count"); do
         echo "    Generating file $i/$count"
         mkdir -p "$test_folder/$size_in_mb"
         chronic dd if=/dev/urandom of="$test_folder/$size_in_mb/$i" bs=1M count="$size_in_mb" 2>&1
@@ -39,10 +39,16 @@ run_test() {
     sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
     echo "done"
 
-    echo -n "testing $@... "
-    local seconds=$(/usr/bin/time -f "%e" $@ "$TEST_DATA" "$TEST_DEST" 2>&1)
-    local mibps=$(echo "scale=2;($SIZE_MB*$FILE_COUNT)/$seconds" | bc)
-    echo "finished in $seconds""s, avg $mibps MiB/s" 
+    echo -n "testing $*... "
+    /usr/bin/time -o /tmp/wcp_bench_ts_temp -f "%e" $@ "$TEST_DATA"/ "$TEST_DEST"/
+
+    local seconds
+    local total_size
+    local mibps
+    seconds=$(cat /tmp/wcp_bench_ts_temp)
+    total_size=$(du -s -BM "$TEST_DATA" | sed 's/M.*//')
+    mibps=$(echo "scale=2;($total_size)/$seconds" | bc)
+    echo "finished in $seconds""s, avg $mibps MiB/s"
 }
 
 if [ ! -e "$TEST_DATA/done_tag" ]; then
@@ -56,4 +62,6 @@ build
 
 run_test "cp -r"
 run_test "rsync -r"
+
 run_test "./build_test/wcp"
+diff -r "$TEST_DATA" "$TEST_DEST"
