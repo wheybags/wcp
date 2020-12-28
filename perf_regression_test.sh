780003bdaecc2b03b09e66676bacd35c3dfdc727
Tom Mason <wheybags@wheybags.com>
2020-12-28 23:05:51 +0000

    tweak iterations in perf script

--- perf_regression_test.sh
+++ perf_regression_test.sh
@@ -6,9 +6,9 @@ base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 old_perf_file="$base_dir/perf_data/latest"
 
 run_bench() {
-  echo "running 1K test">&2; "$base_dir/bench.sh" --time-only --iterations 10 1K 200000
-  echo "running 1M test">&2; "$base_dir/bench.sh" --time-only --iterations 10 1M 7000
-  echo "running 512M test">&2; "$base_dir/bench.sh" --time-only --iterations 5 512M 20
+  echo "running 1K test">&2; "$base_dir/bench.sh" --time-only --iterations 7 1K 200000
+  echo "running 1M test">&2; "$base_dir/bench.sh" --time-only --iterations 7 1M 7000
+  echo "running 512M test">&2; "$base_dir/bench.sh" --time-only --iterations 7 512M 20
 }
 
 new_perf_file="$(mktemp)"
